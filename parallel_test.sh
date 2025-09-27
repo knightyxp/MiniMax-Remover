f2dd5ebabcec77b5a63f5d0da8278fa6afe3bea5
Xiangpeng Yang <knightyxp@gmail.com>
2025-09-28 00:52:40 +1000

    sync virga

---
+++ parallel_test.sh
@@ -0,0 +1,6 @@
+python parallel_minimax_remover.py \
+    --test_json test_json/test.json \
+    --base_path /projects/D2DCRC/xiangpeng/Filter_Video_In_context_data \
+    --model_path ./minimax-remover \
+    --video_length 81 \
+    --iterations 6
